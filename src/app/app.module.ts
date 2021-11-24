b9b83378aadd69b0dfeb53181dd6730efadd5290
Falarad <pimentel611@gmail.com>
2021-11-23 21:15:30 -0500

    create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MediaComponent } from './media/media.component';
+import { ManifestoComponent } from './manifesto/manifesto.component';
+import { NotesComponent } from './notes/notes.component';
+import { ReferencesComponent } from './references/references.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaComponent,
+    ManifestoComponent,
+    NotesComponent,
+    ReferencesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/manifesto/manifesto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manifesto',
+  templateUrl: './manifesto.component.html',
+  styleUrls: ['./manifesto.component.sass']
+})
+export class ManifestoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
